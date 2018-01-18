45eb197f8e2ce9d8a93f3099ad42c8aa41e20a92
Eugene Fenko <Eugene Fenko>
2018-01-18 18:27:35 +0200

    upd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert('Hi');
+let addBtn = document.querySelector("input#add");
+let clrBtn = document.querySelector("input#clr");
+let field = document.getElementById('field');
+
+const li = document.createElement('li');
+li.textContent = field.value;
+ 
+
+
+addBtn.addEventListener("click",function() {
+    document.querySelector("ul").appendChild(li);
+    console.log(document.querySelector("ul").chidren);
+});
+
+
+
+
+console.log(li.textContent);
