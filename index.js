1916169d453ddd617f69fe040dc702a8a3c465db
EugeneFenko <gt60@ya.ru>
2018-01-20 18:48:23 +0200

    UPD

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 let addBtn = document.querySelector("input#add");
 let clrBtn = document.querySelector("input#clr");
 let field = document.getElementById('field');
+let form = document.querySelector("form");
+let list = document.querySelector("ul");
 
-const li = document.createElement('li');
-li.textContent = field.value;
- 
+// Add items
+addBtn.addEventListener("click", function (e) {
+    const del = document.createElement('i');
+    const li = document.createElement('li');
 
-
-addBtn.addEventListener("click",function() {
-    document.querySelector("ul").appendChild(li);
-    console.log(document.querySelector("ul").chidren);
+    li.textContent = field.value;
+    li.className = "itemLi";
+    
+    del.textContent = 'X';
+    del.className = 'del';
+    li.appendChild(del); //add del icon
+    
+    
+    if (field.value != "") {
+        list.appendChild(li);   //add item on list
+    }
+    
+    field.value = '';
+    e.preventDefault();
 });
 
+// Clear all items
+clrBtn.addEventListener("click", function () {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+});
 
-
-
-console.log(li.textContent);
+// Delete item 
+document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("del")) {
+        console.log("Deleted")
+        e.target.parentElement.remove();
+    }
+});
